6359b5d75eaee8e0dbf4d3daf436c24b40d187a0
= <neo.ahlander@outlook.com>
2025-05-17 11:34:56 +0200

    Add startup and routes in main

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"net/http"
 
+	"github.com/N30A/personal-website/handlers"
+)
+
+const (
+	Host = "0.0.0.0"
+	Port = "8080"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	handlers.MapStaticFiles(mux)
+	handlers.MapRoutes(mux)
+
+	address := net.JoinHostPort(Host, Port)
+	log.Printf("listening on %s\n", address)
+	log.Fatalln(http.ListenAndServe(address, mux))
 }
