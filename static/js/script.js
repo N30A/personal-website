a1a74ca37c5f950893bebee5ab3f2597e2ac2799
= <neo.ahlander@outlook.com>
2025-06-15 12:01:42 +0200

    Improved theme logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,18 @@
-(function () {
-  try {
-    if (localStorage.getItem("theme") === "dark") {
-      document.documentElement.classList.add("dark");
-    }
-  } catch (e) { }
-})();
+const toggleButton = document.getElementById("theme-toggle");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.getElementById("theme-toggle");
+if (document.documentElement.classList.contains("dark")) {
+  toggleButton.textContent = "🌑";
+} else {
+  toggleButton.textContent = "🌕";
+}
 
+toggleButton.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
   if (document.documentElement.classList.contains("dark")) {
-    toggleButton.textContent = "🌑";
+    localStorage.setItem("theme", "dark");
+    toggleButton.textContent = "🌑"
   } else {
-    toggleButton.textContent = "🌕";
+    localStorage.setItem("theme", "light");
+    toggleButton.textContent = "🌕"
   }
-
-  toggleButton.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
-    if (document.documentElement.classList.contains("dark")) {
-      localStorage.setItem("theme", "dark");
-      toggleButton.textContent = "🌑"
-    } else {
-      localStorage.setItem("theme", "light");
-      toggleButton.textContent = "🌕"
-    }
-  });
 });
